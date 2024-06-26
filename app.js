aa7545112b28fab9c8fed44ad3bed2040b795bdc
ashjoy7 <ashjoy7@gmail.com>
2024-06-25 19:55:32 -0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: './env/.env' });
+require('dotenv').config({ path: './env/.env' });
 
 const express = require('express');
 const bodyParser = require('body-parser');
