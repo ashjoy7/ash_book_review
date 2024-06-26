63bad5127a2e27e95e3424d219fb8fa1fb235f25
ashjoy7 <ashjoy7@gmail.com>
2024-06-26 01:28:57 -0700

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(session({
-  secret: process.env.GOOGLE_CLIENT_SECRET,
+  secret: process.env.SECRET,  // Ensure SECRET is set in your environment variables
   resave: false,
   saveUninitialized: true,
   store: MongoStore.create({ mongoUrl: process.env.MONGO_URI })
