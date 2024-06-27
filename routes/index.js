06a3d556871a0f7055eb58db0462a266d4b439aa
ashjoy7 <ashjoy7@gmail.com>
2024-06-26 23:21:51 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ router.use('/books', require('./bookRoutes'));
 router.use('/reviews', require('./reviewRoutes'));
 router.use('/', require('./authRoutes'));
 
+// Export the router first
 module.exports = router;
+
+// Define your root endpoint handler after exporting the router
 router.get('/', (req, res) => {
   res.send('Welcome to the Book Review API');
 });
-
