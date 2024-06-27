c456f955d091bd5abff30851f0baaa175c566da9
ashjoy7 <ashjoy7@gmail.com>
2024-06-26 22:44:25 -0700

    update

--- swagger.js
+++ swagger.js
@@ -3,13 +3,13 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'Ash Book Review API',
-    description: 'API for managing book reviews',
+    description: 'API for managing book reviews'
   },
-  host: 'https://ash-book-review.onrender.com', // Update this with your actual host URL where your Node.js app is running
-  schemes: ['https'], // Update to ['https'] if your server uses HTTPS
+  host: 'https://ash-book-review.onrender.com', 
+  schemes: ['https'] 
 };
 
 const outputFile = './swagger-output.json';
-const endpointsFiles = ['./routes/index.js']; // Update with your actual API routes file
+const endpointsFiles = ['./routes/index.js'];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
