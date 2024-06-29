1b62d70afacd804247b86598e6479e846fb5ad9b
ashjoy7 <ashjoy7@gmail.com>
2024-06-29 13:34:46 -0700

    update

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const bookController = require('../controllers/bookController');
 const reviewController = require('../controllers/reviewController');
 
-// Route to get all reviews associated with a specific book
-router.get('/books/:bookId/reviews', reviewController.getAllReviews);
-
-// Route to get a specific review by its ID
-router.get('/reviews/:reviewId', reviewController.getReviewsById);
-
-// Route to create a new review
-router.post('/books/:bookId/reviews', reviewController.createReview);
-
-// Route to update a review by its ID
-router.put('/:reviewId', reviewController.updateReview);
-
-// Route to delete a review by its ID
+// Review routes
+router.get('/reviews', reviewController.getAllReviews);
+router.get('/reviews/:reviewId', reviewController.getReviewById);
+router.post('/reviews', reviewController.createReview);
+router.put('/reviews/:reviewId', reviewController.updateReview);
 router.delete('/reviews/:reviewId', reviewController.deleteReview);
 
 module.exports = router;
