8804192fe5e55a466f38983e5b218361185ebfc1
ashjoy7 <ashjoy7@gmail.com>
2024-06-29 14:39:59 -0700

    update

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ app.use(passport.session());
 // Logging middleware
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  console.log('Request Body:', req.body); // Log the request body
   next();
 });
 
