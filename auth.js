05da71f61060aa553799540709255d5f630f2806
ashjoy7 <ashjoy7@gmail.com>
2024-07-18 02:26:14 -0700

    update

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.SECRET,
       callbackURL: 'https://ash-book-review.onrender.com/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
