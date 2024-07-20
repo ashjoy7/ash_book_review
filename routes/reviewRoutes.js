f396a67195fc51d0995d2fa8a3accda0e1347508
ashjoy7 <ashjoy7@gmail.com>
2024-07-19 18:10:58 -0700

    update

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const reviewController = require('../controllers/reviewController');
+const reviewController = require('../controllers/reviewController'); // Ensure this path is correct
 
 // Define routes relative to '/reviews'
 router.get('/', reviewController.getAllReviews);
